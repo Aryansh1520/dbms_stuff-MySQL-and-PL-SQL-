e3821e2a198afdea6c3078ca17d4d304bf3f4339
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2022-08-23 13:03:25 +0530

    Create ASSIGNMENT_2.sql

---
+++ ASSIGNMENT_2.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE assignment_2;
+SHOW DATABASES;
+/*
++--------------------+
+| Database           |
++--------------------+
+| assignment_1       |
+| assignment_2       |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+*/
