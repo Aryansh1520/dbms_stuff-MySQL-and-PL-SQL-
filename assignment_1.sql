45f4702556e38498371a0f3de6ea9dae1b736e58
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2022-09-08 18:12:12 +0530

    Create assignment_1.sql

---
+++ assignment_1.sql
@@ -0,0 +1,205 @@
+/*ASSIGMENT 1 + VIEW PART OF ASSIGNMENT 2
+*/
+
+
+
+CREATE DATABASE group_2;
+SHOW DATABASES;
+/*
++--------------------+
+| Database           |
++--------------------+
+| group_2            |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+*/
+
+USE group_2;
+CREATE TABLE student_info (roll_no INT ,name VARCHAR(30),submission_status VARCHAR(30));
+INSERT INTO student_info VALUES(26,'PRANJAL','NA');
+INSERT INTO student_info VALUES(27,'PRASHANT','NA');
+INSERT INTO student_info VALUES(28,'AADARSH','NA');
+INSERT INTO student_info VALUES(29,'TEJAS','NA');
+INSERT INTO student_info VALUES(30,'OM','NA');
+INSERT INTO student_info VALUES(31,'HEMANT','NA');
+INSERT INTO student_info VALUES(32,'DHANASHREE','NA');
+INSERT INTO student_info VALUES(33,'UTKARSH','NA');
+
+SELECT * FROM student_info;
+
+/*
++---------+------------+-------------------+
+| roll_no | name       | submission_status |
++---------+------------+-------------------+
+|      26 | PRANJAL    | NA                |
+|      27 | PRASHANT   | NA                |
+|      28 | AADARSH    | NA                |
+|      29 | TEJAS      | NA                |
+|      30 | OM         | NA                |
+|      31 | HEMANT     | NA                |
+|      32 | DHANASHREE | NA                |
+|      33 | UTKARSH    | NA                |
++---------+------------+-------------------+
+*/
+ALTER TABLE student_info ADD PRIMARY KEY(roll_no);
+
+desc student_info;
+/*
++-------------------+-------------+------+-----+---------+-------+
+| Field             | Type        | Null | Key | Default | Extra |
++-------------------+-------------+------+-----+---------+-------+
+| roll_no           | int         | NO   | PRI | NULL    |       |
+| name              | varchar(30) | YES  |     | NULL    |       |
+| submission_status | varchar(30) | YES  |     | NULL    |       |
++-------------------+-------------+------+-----+---------+-------+
+*/
+
+ALTER TABLE student_info ADD COLUMN teacher_name VARCHAR(30) DEFAULT  'DEEPALI';
+
+/*
++---------+------------+-------------------+--------------+
+| roll_no | name       | submission_status | teacher_name |
++---------+------------+-------------------+--------------+
+|      26 | PRANJAL    | NA                | DEEPALI      |
+|      27 | PRASHANT   | NA                | DEEPALI      |
+|      28 | AADARSH    | NA                | DEEPALI      |
+|      29 | TEJAS      | NA                | DEEPALI      |
+|      30 | OM         | NA                | DEEPALI      |
+|      31 | HEMANT     | NA                | DEEPALI      |
+|      32 | DHANASHREE | NA                | DEEPALI      |
+|      33 | UTKARSH    | NA                | DEEPALI      |
++---------+------------+-------------------+--------------+
+
+*/
+
+UPDATE student_info SET submission_status = 'COMPLETED' WHERE roll_no = 31;
+
+/*
+
++---------+------------+-------------------+--------------+
+| roll_no | name       | submission_status | teacher_name |
++---------+------------+-------------------+--------------+
+|      26 | PRANJAL    | NA                | DEEPALI      |
+|      27 | PRASHANT   | NA                | DEEPALI      |
+|      28 | AADARSH    | NA                | DEEPALI      |
+|      29 | TEJAS      | NA                | DEEPALI      |
+|      30 | OM         | NA                | DEEPALI      |
+|      31 | HEMANT     | COMPLETED         | DEEPALI      |
+|      32 | DHANASHREE | NA                | DEEPALI      |
+|      33 | UTKARSH    | NA                | DEEPALI      |
++---------+------------+-------------------+--------------+
+/*
+
+UPDATE student_info SET submission_status = 'COMPLETED' WHERE name = 'DHANASHREE';
+/*
++---------+------------+-------------------+--------------+
+| roll_no | name       | submission_status | teacher_name |
++---------+------------+-------------------+--------------+
+|      26 | PRANJAL    | NA                | DEEPALI      |
+|      27 | PRASHANT   | NA                | DEEPALI      |
+|      28 | AADARSH    | NA                | DEEPALI      |
+|      29 | TEJAS      | NA                | DEEPALI      |
+|      30 | OM         | NA                | DEEPALI      |
+|      31 | HEMANT     | COMPLETED         | DEEPALI      |
+|      32 | DHANASHREE | COMPLETED         | DEEPALI      |
+|      33 | UTKARSH    | NA                | DEEPALI      |
++---------+------------+-------------------+--------------+
+*/
+
+INSERT INTO student_info (roll_no,name,submission_status) VALUES (34,'XYZ','NA');
+
+/*
++---------+------------+-------------------+--------------+
+| roll_no | name       | submission_status | teacher_name |
++---------+------------+-------------------+--------------+
+|      26 | PRANJAL    | NA                | DEEPALI      |
+|      27 | PRASHANT   | NA                | DEEPALI      |
+|      28 | AADARSH    | NA                | DEEPALI      |
+|      29 | TEJAS      | NA                | DEEPALI      |
+|      30 | OM         | NA                | DEEPALI      |
+|      31 | HEMANT     | COMPLETED         | DEEPALI      |
+|      32 | DHANASHREE | COMPLETED         | DEEPALI      |
+|      33 | UTKARSH    | NA                | DEEPALI      |
+|      34 | XYZ        | NA                | DEEPALI      |
++---------+------------+-------------------+--------------+
+*/
+
+DELETE FROM student_info WHERE name = 'XYZ';
+
+/*
++---------+------------+-------------------+--------------+
+| roll_no | name       | submission_status | teacher_name |
++---------+------------+-------------------+--------------+
+|      26 | PRANJAL    | NA                | DEEPALI      |
+|      27 | PRASHANT   | NA                | DEEPALI      |
+|      28 | AADARSH    | NA                | DEEPALI      |
+|      29 | TEJAS      | NA                | DEEPALI      |
+|      30 | OM         | NA                | DEEPALI      |
+|      31 | HEMANT     | COMPLETED         | DEEPALI      |
+|      32 | DHANASHREE | COMPLETED         | DEEPALI      |
+|      33 | UTKARSH    | NA                | DEEPALI      |
++---------+------------+-------------------+--------------+
+
+*/
+
+
+CREATE VIEW a_view AS SELECT roll_no , submission_status FROM student_info;
+SELECT * FROM a_view;
+/*
++---------+-------------------+
+| roll_no | submission_status |
++---------+-------------------+
+|      26 | NA                |
+|      27 | NA                |
+|      28 | NA                |
+|      29 | NA                |
+|      30 | NA                |
+|      31 | COMPLETED         |
+|      32 | COMPLETED         |
+|      33 | NA                |
++---------+-------------------+
+*/
+ALTER VIEW a_view AS SELECT name FROM student_info;
+SELECT * FROM a_view;
+/*
++------------+
+| name       |
++------------+
+| PRANJAL    |
+| PRASHANT   |
+| AADARSH    |
+| TEJAS      |
+| OM         |
+| HEMANT     |
+| DHANASHREE |
+| UTKARSH    |
++------------+
+*/
+UPDATE a_view SET name = 'OM GAWANDE' WHERE name = 'OM';
+/*
++------------+
+| name       |
++------------+
+| PRANJAL    |
+| PRASHANT   |
+| AADARSH    |
+| TEJAS      |
+| OM GAWANDE |
+| HEMANT     |
+| DHANASHREE |
+| UTKARSH    |
++------------+
+*/
+DROP VIEW a_view;
+
+
+
+
+
+
+
+
+
