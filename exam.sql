3b0f377fe859613cb9699e1b8b23c05702c3c16d
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2022-09-12 15:57:59 +0530

    Create exam.sql

---
+++ exam.sql
@@ -0,0 +1,29 @@
+--LOAN TABLE
+/*
++---------+-------------+--------+
+| loan_no | branch_name | amount |
++---------+-------------+--------+
+|       1 | MUMBAI      |    100 |
+|       2 | PUNE        |   2000 |
+|       3 | AHEMADABAD  |   2000 |
+|       4 | PUNE        |    800 |
++---------+-------------+--------+
+*/
+--Borrower TABLE
+/*
++-----------+---------+
+| cust_name | loan_no |
++-----------+---------+
+| ASHUTOSH  |       1 |
+| ARYAN     |       2 |
+| SAHIL     |       3 |
+| VIRAG     |       4 |
++-----------+---------+
+/*
+REQUIRED QUERY
+
+a) SELECT loan.loan_no,Borrower.cust_name,loan.amount FROM loan,Borrower WHERE loan.loan_no = Borrower.loan_no;
+b) SELECT  Borrower.cust_name FROM Borrower,loan WHERE loan.loan_no = Borrower.loan_no AND loan.branch_name = 'PUNE' ORDER BY Borrower.cust_name ;
+c) SELECT loan.loan_no FROM loan WHERE branch_name = 'PUNE' AND loan.loan_amount >2000;
+
+
