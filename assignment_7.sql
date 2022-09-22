dc63458e03475791c815da3f3518627be321b18e
Aryan Sharma <81407258+Aryansh1520@users.noreply.github.com>
2022-09-22 14:50:13 +0530

    Update assignment_7.sql

--- assignment_7.sql
+++ assignment_7.sql
@@ -3,24 +3,31 @@ CREATE TABLE lib_tab(book_name VARCHAR2(25),status VARCHAR2(15));
 CREATE TABLE library_audit(date_modified DATE, book_name VARCHAR2(25),old_status VARCHAR(15),new_status VARCHAR2(15),action VARCHAR2(25));
 */
 
-
 CREATE OR REPLACE TRIGGER trigger_1 
-AFTER UPDATE OR DELETE ON lib_tab FOR EACH ROW
+AFTER UPDATE OR DELETE OR INSERT ON lib_tab FOR EACH ROW
 ENABLE
-	DECLARE
-		aud_bookname lib_tab.book_name%TYPE;
 	BEGIN
 		IF UPDATING THEN
-			aud_bookname := :OLD.book_name;
 			dbms_output.put_line(:OLD.status);
-			INSERT INTO library_audit VALUES (SYSDATE,aud_bookname,:OLD.status,:NEW.status,'UPDATE');
+			INSERT INTO library_audit VALUES (SYSDATE,:OLD.book_name,:OLD.status,:NEW.status,'UPDATE');
+                ELSIF INSERTING THEN
+                         dbms_output.put_line(:NEW.status);
+                         INSERT INTO library_audit VALUES (SYSDATE,:NEW.book_name,:OLD.status,:NEW.status,'INSERT');
 		ELSE
 			dbms_output.put_line(:OLD.book_name||'deleting');
-			INSERT INTO library_audit VALUES(SYSDATE,:OLD.book_name,:OLD.status,NULL,'DELETE');
+			INSERT INTO library_audit VALUES(SYSDATE,:OLD.book_name,:OLD.status,:NEW.status,'DELETE');
 		END IF;
 	END;
 /
-
+/*
+DELETE FROM lib_tab WHERE book_name = 'SILENT HILL';
+UPDATE lib_tab SET status = 'UNAVAILABLE' WHERE book_name =  'UNCHARTED';
+UPDATE lib_tab SET status = 'PRE-ORDER' WHERE book_name = 'GOD OF WAR';
+UPDATE lib_tab SET status = 'AVAILABLE' WHERE book_name = 'UNCHARTED';
+INSERT INTO lib_tab VALUES('SPM','UNAVAILABLE');
+Select * from library_audit;
+Select * from lib_tab;
+*/
 
 /*
 INSERT INTO lib_tab VALUES('DARK MATTER','AVAILABLE');
